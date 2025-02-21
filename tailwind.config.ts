f263080ec0d39fc8d681888bb24d268d50833bdd
MeechyFZ <ponsnicolas7@gmail.com>
2025-02-21 11:17:17 +0100

    Correction ts config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,67 +1,43 @@
 import type { Config } from "tailwindcss";
-const svgToDataUri = require("mini-svg-data-uri");
-const { default: flattenColorPalette } = require("tailwindcss/lib/util/flattenColorPalette");
 
 export default {
-  darkMode: ["class"],
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      container: {
-        center: true,
-        padding: "15px",
-      },
-      colors: {
-        primary: "#1c1c22",
-        secondary: "#23232B",
-        accent: {
-          DEFAULT: "#00ff99",
-          hover: "#00e187",
-        },
-      },
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
-      screens: {
-        sm: "640px", // Mobile
-        md: "768px", // Tablette
-        lg: "960px", // Desktop
-        xl: "1200px", // Grand écran
-      },
-      fontFamily: {
-        primary: "var(--font-jetbrainsMono)",
-      },
-    },
+  	extend: {
+		container : {
+			center : true,
+			padding : "15px",
+		},
+  		colors: {
+ 			primary : '#1c1c22',
+			secondary: '#23232B',
+			accent: {
+				DEFAULT: '#00ff99',
+				hover: '#00e187'
+			}
+			
+
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+		  screens: {
+			sm: '640px', // Mobile
+			md: '768px', // Tablette
+			lg: '960px', // Desktop
+			xl: '1200px', // Grand écran
+		 },
+		 fontFamily: {
+			primary: "var(--font-jetbrainsMono)", 
+		 }
+  	}
   },
-  plugins: [
-    require("tailwindcss-animate"),
-    function ({ matchUtilities , theme }) {
-      matchUtilities(
-        {
-          "bg-grid": (value :string ) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-grid-small": (value:string) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
-            )}")`,
-          }),
-          "bg-dot": (value:string ) => ({
-            backgroundImage: `url("${svgToDataUri(
-              `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
-            )}")`,
-          }),
-        },
-        { values: flattenColorPalette(theme("colors")), type: "color" }
-      );
-    },
-  ],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
