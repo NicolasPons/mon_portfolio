f5e516fdd26b4e3cca9a0413b3f7a5fb694a27c1
MeechyFZ <ponsnicolas7@gmail.com>
2025-04-23 13:39:04 +0200

    Debug veille 2

--- components/Veille.tsx
+++ components/Veille.tsx
@@ -2,7 +2,7 @@ export default function VeilleTechnologique() {
   return (
     <div className="w-full h-screen">
       <iframe
-        src="https://dotnet-veilletechno.my.canva.site/"
+        src="https://www.canva.com/design/DAGlXAj5OKk/0Nv7hhLK_a6ocPjw4hayyw/view"
         width="100%"
         height="100%"
         style={{ border: "none" }}
